848fbc77761d5cac16c57800b2d3d3ff752f2215
Blannck <ajunielfrancislae@gmail.com>
2025-11-17 11:37:54 +0800

    error fix for vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,6 +8,10 @@ export const prisma =
 globalForPrisma.prisma ??
 new PrismaClient({
 log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+transactionOptions: {
+maxWait: 15000, // Maximum time to wait to start a transaction (15s)
+timeout: 30000, // Maximum time a transaction can run (30s)
+},
 });
 
 
